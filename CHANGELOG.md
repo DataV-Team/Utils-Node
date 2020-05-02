9358c82273439318dd6d61a346039c40ddc853ff
jiaming743 <743192023@qq.com>
2020-05-02 15:31:39 +0800

    update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+### 0.1.0 (2020-05-02)
+
+- **getNodeParams:** getNodeParams renamed as getCmdParams.
+
 ### Release (2020-05-02)
 
 - **release:** node-utils.
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
-export function getNodeParams(): any {
+export function getCmdParams(): any {
   // eslint-disable-next-line
   return process.argv.slice(2).reduce((params: any, param) => {
     const [key, val] = param.split('=')
